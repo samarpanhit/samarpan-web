02ef712b6f65859ef8cea6b7b6fe554755c68878
Neeraj Kumar <nk4976744@gmail.com>
2022-05-20 23:33:33 +0530

    adding event info

--- src/Component/Home/OurEvents/EventsFullPage.js
+++ src/Component/Home/OurEvents/EventsFullPage.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useParams } from 'react-router';
 import Events_db from '../../../Database/Events_db.json'
-
+import img1 from '../../../assets/slider/1.jpg'
+import './eventfullpage.css'
 const EventsFullPage = () => {
   // console.log(window.location.pathname);
   
   return (
     <div>
-       COMMING SOON
+      <div className='Event'>
+        <div className='event-pic' style={{ backgroundImage: "url(" + img1 + ")" }}>
+        </div>
+        <div className='event-info'>
+        <p className='date'>25 JUNE 2022</p>
+        <h3>What is lorem ipsum</h3>
+        <p className='event-data'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
     </div>
   )
 }
